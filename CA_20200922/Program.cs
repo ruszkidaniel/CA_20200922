16547b199997c5742096276d6e9b5747e408368f
Dániel Ruszki <ruszki.daniel@verebelyszki.hu>
2020-09-22 10:18:26 +0200

    init

---
+++ CA_20200922/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CA_20200922
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Ruszki Dániel");
+            Console.ReadKey();
+        }
+    }
+}
